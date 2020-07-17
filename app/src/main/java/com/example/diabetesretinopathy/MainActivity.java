1de19e26710382603702dc832e4873ca7083a80a
mainamuriuki274 <muriukimainalewis.com>
2020-07-17 21:13:01 +0300

    Added MainActivity page

--- app/src/main/java/com/example/diabetesretinopathy/MainActivity.java
+++ app/src/main/java/com/example/diabetesretinopathy/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.diabetesretinopathy;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.FrameLayout;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    FrameLayout frameLayout;
-
+public class MainActivity extends Activity {
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.take_photo);
+        btn.setOnClickListener(new OnClickListener() {
 
+            public void onClick(View v) {
+                Intent i=new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                startActivity(i);
+            }
+        });
     }
 
 }
