859cd3065a2d0b50fd34da1a58a68afead93da96
mainamuriuki274 <muriukimainalewis.com>
2020-07-17 21:39:06 +0300

    Added upload from gallery feature

--- app/src/main/java/com/example/diabetesretinopathy/MainActivity.java
+++ app/src/main/java/com/example/diabetesretinopathy/MainActivity.java
@@ -1,27 +1,59 @@
 package com.example.diabetesretinopathy;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
-    Button btn;
+    Button takephoto,upload;
+    ImageView viewImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn=(Button)findViewById(R.id.take_photo);
-        btn.setOnClickListener(new OnClickListener() {
+        takephoto = (Button)findViewById(R.id.take_photo);
+        takephoto.setOnClickListener(new OnClickListener() {
 
             public void onClick(View v) {
                 Intent i=new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
                 startActivity(i);
             }
         });
-    }
 
+        upload = (Button)findViewById(R.id.upload_photo);
+        upload.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new   Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 2);
+            }
+        });
+    }
+    @SuppressLint("LongLogTag")
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 2) {
+            Uri selectedImage = data.getData();
+            String[] filePath = { MediaStore.Images.Media.DATA };
+            Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
+            c.moveToFirst();
+            int columnIndex = c.getColumnIndex(filePath[0]);
+            String picturePath = c.getString(columnIndex);
+            c.close();
+            Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+            Log.w("path of image from gallery", picturePath+"");
+            //viewImage.setImageBitmap(thumbnail);
+        }
+    }
 }
