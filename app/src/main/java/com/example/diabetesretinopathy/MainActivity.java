69ecda096c00c6a096a31acc9c3f6fcbd4a0edb8
mainamuriuki274 <muriukimainalewis.com>
2020-07-17 14:07:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/diabetesretinopathy/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.diabetesretinopathy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+    FrameLayout frameLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
