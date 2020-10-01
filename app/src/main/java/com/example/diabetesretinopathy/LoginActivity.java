a03e5f53ecc8ae87f7d2fea8b59a12f9a3b5228e
mainamuriuki274 <muriukimainalewis.com>
2020-10-02 01:33:27 +0300

    Tweaked UI a little

--- app/src/main/java/com/example/diabetesretinopathy/LoginActivity.java
+++ app/src/main/java/com/example/diabetesretinopathy/LoginActivity.java
@@ -2,13 +2,35 @@ package com.example.diabetesretinopathy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button sign_in;
+    TextView sign_up;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+            sign_in = (Button)findViewById(R.id.sign_in);
+        sign_in.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent i=new Intent(LoginActivity.this,MainActivity.class);
+               startActivity(i);
+            }
+        });
+        sign_up = (TextView)findViewById(R.id.signup_link);
+        sign_up.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent i=new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/diabetesretinopathy/SignupActivity.java
+++ app/src/main/java/com/example/diabetesretinopathy/SignupActivity.java
@@ -2,13 +2,25 @@ package com.example.diabetesretinopathy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
-
+    TextView sign_in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        getSupportActionBar().hide();
+        sign_in = (TextView)findViewById(R.id.signin_link);
+        sign_in.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent i=new Intent(SignupActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
