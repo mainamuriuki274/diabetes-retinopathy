e68c1f84d6354ad02788a01479e6dd1fc1c3478d
mainamuriuki274 <muriukimainalewis.com>
2020-10-12 23:51:50 +0300

    Did some backend on SettingsActivity

--- app/src/main/java/com/example/diabetesretinopathy/SettingsActivity.java
+++ app/src/main/java/com/example/diabetesretinopathy/SettingsActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
@@ -15,10 +16,15 @@ import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Objects;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -27,11 +33,14 @@ public class SettingsActivity extends AppCompatActivity {
     ImageView mNotificationImage,mHelpImage,mAboutImage;
     Switch mNotificationSwitch;
     LinearLayout mLastReminder,mNextReminder;
+    TextView mLast_Reminder,mNext_Reminder;
     TextView mTextNotification,mUserName,mUserEmail;
     SharedPreferences sharedPreferences;
     public static final String MyPREFERENCES = "user" ;
     public static final String Name = "nameKey";
     public static final String Email = "emailKey";
+    public static final String Notification = "notificationKey";
+    public static final String Timeline = "timelineKey";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +59,8 @@ public class SettingsActivity extends AppCompatActivity {
         mNotificationBtn    = findViewById(R.id.notification_card);
         mNotificationImage  = findViewById(R.id.imageViewNotification);
         mNotificationSwitch = findViewById(R.id.notification_switch);
+        mLast_Reminder      = findViewById(R.id.last_reminder);
+        mNext_Reminder       = findViewById(R.id.next_reminder);
         mLastReminder       = findViewById(R.id.LastReminder);
         mNextReminder       = findViewById(R.id.NextReminder);
         mTextNotification   = findViewById(R.id.text_notification);
@@ -61,18 +72,101 @@ public class SettingsActivity extends AppCompatActivity {
         mNextReminder.setVisibility(View.GONE);
         mLastReminder.setVisibility(View.GONE);
 
-        sharedPreferences   = this.getSharedPreferences(MyPREFERENCES, MODE_PRIVATE);
-        String username     = sharedPreferences.getString(Name, "");
-        String email     = sharedPreferences.getString(Email, "");
-        if(username != null && email != null){
-            mUserName.setText(username);
-            mUserEmail.setText(email);
-        }
+        sharedPreferences      = this.getSharedPreferences(MyPREFERENCES, MODE_PRIVATE);
+        String username        = sharedPreferences.getString(Name, "");
+        String email           = sharedPreferences.getString(Email, "");
+        String timeline        = sharedPreferences.getString(Timeline, "");
+        String notification   = sharedPreferences.getString(Notification,"");
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this, R.array.planets_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mSpinner.setAdapter(adapter);
+
+        if(username != null && email != null && timeline != null ){
+            mUserName.setText(username);
+            mUserEmail.setText(email);
+            mSpinner.setSelection(adapter.getPosition(timeline));
+            mNotificationSwitch.setChecked(Boolean.parseBoolean(notification));
+        }
+
+       if(mNotificationSwitch.isChecked()){
+           mTextNotification.setVisibility(View.VISIBLE);
+           mSpinner.setVisibility(View.VISIBLE);
+           mNextReminder.setVisibility(View.VISIBLE);
+           mLastReminder.setVisibility(View.VISIBLE);
+       }
+
+        mNotificationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                if(isChecked) {
+                    editor.putString(Notification,"true");
+                    if(editor.commit()) {
+                        mTextNotification.setVisibility(View.VISIBLE);
+                        mSpinner.setVisibility(View.VISIBLE);
+                        mNextReminder.setVisibility(View.VISIBLE);
+                        mLastReminder.setVisibility(View.VISIBLE);
+                    }
+
+                } else {
+                    editor.putString(Notification,"false");
+                    if(editor.commit()) {
+                        mTextNotification.setVisibility(View.GONE);
+                        mSpinner.setVisibility(View.GONE);
+                        mNextReminder.setVisibility(View.GONE);
+                        mLastReminder.setVisibility(View.GONE);
+                    }
+                }
+            }
+        });
+
+
+
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                String notification_timeline = mSpinner.getSelectedItem().toString().trim();
+                editor.putString(Timeline,notification_timeline);
+                if(editor.commit()) {
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                    Calendar c = Calendar.getInstance();
+                    String date = mLast_Reminder.getText().toString();
+                    Date date2= null;
+                    try {
+                        date2 = sdf.parse(date);
+
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    if(notification_timeline.equals("1 month")) {
+                        c.setTime(date2);
+                        c.add(Calendar.DATE, 30);
+                        String output = sdf.format(c.getTime());
+                        mNext_Reminder.setText(output);
+                    }
+                    else if(notification_timeline.equals("2 months")) {
+                        c.setTime(date2);
+                        c.add(Calendar.DATE, 60);
+                        String output = sdf.format(c.getTime());
+                        mNext_Reminder.setText(output);
+                    }
+                    else if(notification_timeline.equals("3 months")) {
+                        c.setTime(date2);
+                        c.add(Calendar.DATE, 90);
+                        String output = sdf.format(c.getTime());
+                        mNext_Reminder.setText(output);
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
 
         mAccount.setOnClickListener(new View.OnClickListener() {
@@ -122,27 +216,6 @@ public class SettingsActivity extends AppCompatActivity {
                 else{
                     mAbout.setVisibility(View.VISIBLE);
                     mAboutImage.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.up_arrow));
-                }
-            }
-        });
-
-
-
-
-        mNotificationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked) {
-                    mTextNotification.setVisibility(View.VISIBLE);
-                    mSpinner.setVisibility(View.VISIBLE);
-                    mNextReminder.setVisibility(View.VISIBLE);
-                    mLastReminder.setVisibility(View.VISIBLE);
-
-                } else {
-                    mTextNotification.setVisibility(View.GONE);
-                    mSpinner.setVisibility(View.GONE);
-                    mNextReminder.setVisibility(View.GONE);
-                    mLastReminder.setVisibility(View.GONE);
                 }
             }
         });
